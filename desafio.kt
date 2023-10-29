1284db360f986c5e85c54b677509b5ce3f4d84c6
YamaneNobuo <142440653+YamaneNobuo@users.noreply.github.com>
2023-10-29 07:27:10 -0300

    Update desafio.kt inserções de código

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario (val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class ConteudoEducacional<E>(vararg items: E) {              // 1
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+  private val elements = items.toMutableList()
 
+  fun push(element: E) = elements.add(element)        // 2
+
+  fun peek(): E = elements.last()                     // 3
+
+  fun pop(): E = elements.removeAt(elements.size - 1)
+
+  fun isEmpty() = elements.isEmpty()
+
+  fun size() = elements.size
+
+  override fun toString() = "ConteudoEducacional(${elements.joinToString()})"
+}
+
+data class Formacao(val nome: String, val duracao: Int, var conteudos: String) {
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario): String {
+        println (usuario)
+        println (inscritos)
+        return ("Matricula para usuario $usuario efetuada.")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario = Usuario("BONGO")
+    println(usuario.nome)
+    
+    val tipoConteudo: Int = 3  // informar qual o conteúdo da formação
+    println(tipoConteudo)
+    
+    val conteudoEducacional = ConteudoEducacional("Classes CSS", "HTML")
+    if (tipoConteudo == 1) {
+       conteudoEducacional.push("Java")
+    }
+    if (tipoConteudo == 2) {
+       conteudoEducacional.push(".Net")
+    }
+    if (tipoConteudo == 3) {
+       conteudoEducacional.push("kotlin")
+    }
+       
+    println(conteudoEducacional)
+    
+    val conteudo = conteudoEducacional
+    println(conteudo)
+    
 }
